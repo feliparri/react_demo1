85cdf19b5edaf952ae4731b7f46328c702ad55b1
felipe huerta iturra <felipehuertaiturra@MacBook-Pro-de-felipe.local>
2019-01-27 00:41:35 -0300

    inicio de la demo1 estudiando react

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Grid } from "react-bootstrap";
+
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Route from "react-router-dom/Route";
 import { Inicio } from "./Inicio";
@@ -6,25 +8,28 @@ import { Saludo } from "./Saludo";
 import { Pelicula } from "./Pelicula";
 import { Menu } from "./Menu";
 import { PaginaNoExiste } from "./PaginaNoExiste";
+import Moment from "react-moment";
 
 export class App extends Component {
   render() {
+    const { nombre } = this.props;
+    const { mensaje } = this.props;
+    const dateToFormat = new Date();
     return (
-      <Router>
-        <div>
-          <Menu />
-          <Switch>
-            <Route path="/" component={Inicio} exact />
-            <Route path="/saludar" component={Saludo}>
-              Saludar
-            </Route>
-            <Route path="/peliculas" component={Pelicula} />
-            <Route component={PaginaNoExiste} />
-          </Switch>
-        </div>
-      </Router>
+      <Grid>
+        <h1>Hola {nombre}!</h1>
+        <h6>{mensaje}!</h6>
+        <Moment interval={0} format="YYYY/MM/DD">
+          {dateToFormat}
+        </Moment>
+      </Grid>
     );
   }
 }
+
+App.defaultProps = {
+  nombre: "Default",
+  mensaje: "mensaje pasado por parametro por default"
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import App from "./componentes/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 
+import Moment from "react-moment";
+import "moment-timezone";
+
 ReactDOM.render(
-  <App nombre="Edgar" mensaje="Este es un mensaje desde los parámetros." />,
+  <App
+  //nombre="Felipe"
+  //mensaje="mensaje enviado desde index"
+  //hora= Date().toLocaleString()
+  />,
   document.getElementById("root")
 );
