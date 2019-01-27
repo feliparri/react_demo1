f1143e8a9a8d8c3470f1891425427499154e1a38
felipe huerta iturra <felipe.huerta.iturra@gmail.com>
2019-01-27 01:13:57 -0300

    add: funciones, variables, constantes, envio de formularios, prevent default, etc

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Grid } from "react-bootstrap";
+import {
+  Grid,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Button
+} from "react-bootstrap";
 
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import Route from "react-router-dom/Route";
@@ -11,8 +17,23 @@ import { PaginaNoExiste } from "./PaginaNoExiste";
 import Moment from "react-moment";
 
 export class App extends Component {
+  state = {
+    nombre: this.props.nombre
+  };
+
+  alOprimirBoton = e => {
+    e.preventDefault();
+    const nombre = e.target.nombre.value;
+    if (typeof nombre === "string" && nombre.length > 0) {
+      this.setState({
+        nombre: nombre
+      });
+      e.target.nombre.value = "";
+    }
+  };
+
   render() {
-    const { nombre } = this.props;
+    const { nombre } = this.state;
     const { mensaje } = this.props;
     const dateToFormat = new Date();
     return (
@@ -22,6 +43,14 @@ export class App extends Component {
         <Moment interval={0} format="YYYY/MM/DD">
           {dateToFormat}
         </Moment>
+        <form onSubmit={this.alOprimirBoton}>
+          <FormGroup>
+            <ControlLabel>NOMBRE:</ControlLabel>
+            <FormControl type="text" id="nombre" placeholder="ingrese nombre" />
+            <hr />
+            <Button type="submit">Enviar</Button>
+          </FormGroup>
+        </form>
       </Grid>
     );
   }
